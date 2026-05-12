70ebabf374b6809d50bb21f468b0cd55a3951716
shivamkr12072004-coder <shivamkr12072004@gmail.com>
2026-05-12 11:46:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,28 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
     if request.method == 'POST':
 
         subjects = request.form['subjects']
+        hours = request.form['hours']
+        date = request.form['date']
 
-        subjects_list = [s.strip() for s in subjects.split(',')]
+        subject_list = subjects.split(',')
 
         timetable = []
 
-        day = 1
-
-        for sub in subjects_list:
-
-            if day % 2 == 0:
-                timetable.append(
-                    (f"Day {day}", sub, "6:00 AM - 8:00 AM")
-                )
-
-            else:
-                timetable.append(
-                    (f"Day {day}", sub, "7:00 PM - 9:00 PM")
-                )
-
-            day += 1
+        for i, subject in enumerate(subject_list):
+            timetable.append({
+                "day": f"Day {i+1}",
+                "subject": subject.strip(),
+                "time": f"{hours} Hours"
+            })
 
         return render_template(
             'result.html',
-            timetable=timetable
+            timetable=timetable,
+            date=date
         )
 
     return render_template('index.html')
